c9e142b28afbdb74d649b3bc693123c3562460fa
yajvin-dev <yajvin.work@gmail.com>
2025-06-19 09:49:05 +0530

    Create accordian.js
    Added Accordian.js to the project

---
+++ blocks/accordian/accordian.js
@@ -0,0 +1,20 @@
+function decorateAccordion(el) {
+    const titles = el.querySelectorAll(':scope > div:nth-child(odd)');
+    titles.forEach((title) => {
+      // Add a class to the title container
+      title.classList.add('item-title');
+      // Remove the empty div
+      title.querySelector(':scope > div:last-of-type').remove();
+      // Add a class to the content
+      title.nextElementSibling.classList.add('item-content');
+      // Add a click handler to open the content
+      title.addEventListener('click', () => {
+        title.classList.toggle('open');
+      });
+    });
+  }
+  
+  const els = document.querySelectorAll('.accordion');
+  els.forEach((el) => {
+    decorateAccordion(el);
+  });
